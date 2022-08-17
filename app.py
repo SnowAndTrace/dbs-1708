b338b69f3f105f928a5ea7dd233fc5f8e2cc1239
SnowAndTrace <82654911+SnowAndTrace@users.noreply.github.com>
2022-08-17 12:16:13 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,13 +7,11 @@ def index():
     if request.method == "POST":
         rates = float(request.form.get("rates"))
         model1 = joblib.load("regression")
-        r1 = model1.predict[[rates]]
+        r1 = model1.predict([[rates]])
         model2 = joblib.load("tree")
         r2 = model2.predict([[rates]])
-
         print(rates)
-        return(render_template("index.html",result1="r1",result2="r2"))
-
+        return(render_template("index.html",result1=r1,result2=r2))
     else:
         return(render_template("index.html",result1="waiting",result2="waiting"))
 
